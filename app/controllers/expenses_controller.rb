b9595ea1cf60a6aaacfac23e4526d5a0325a2edc
Jasson Rdz <ja7rodal@gmail.com>
2016-10-10 20:45:08 -0500

    adjust filter on controllers

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,19 @@
 class ExpensesController < ApplicationController
   def index
      @expenses = Expense.order("date DESC")
+     unless params[:category_id].blank? && params[:concept].blank?
+       if params[:concept].blank?
+         @expenses = Expense.where(category_id: params[:category_id]).order("date DESC")
+       elsif params[:category_id].blank?
+         @expenses = Expense.where("concept LIKE ?", "%#{params[:concept]}%").order("date DESC")
+       else
+         @expenses = Expense.where("concept LIKE ? AND category_id=?", "%#{params[:concept]}%", params[:category_id]).order("date DESC")
+       end
+
+       #@expenses = Expense.where("concept LIKE ?", "%#{params[:concept]}%").order("date DESC")
+
+     end
+
+
   end
 end
